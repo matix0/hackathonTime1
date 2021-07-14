643982f510e34bcaa6035e53bcf48c8bdebd1a14
matix0 <mateusviniciuspw@gmail.com>
2021-07-13 21:12:07 -0300

    integrate back to front

--- app/src/pages/mainPage/mainPage.tsx
+++ app/src/pages/mainPage/mainPage.tsx
@@ -1,12 +1,12 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import PostBox from "../../components/posts";
-import {useHistory} from 'react-router-dom'
-import { getUserById } from '../../services/users'
-import { ThemeProvider } from 'styled-components';
-import {lightTheme, darkTheme, GlobalStyle} from '../../components/themes'
+import { useHistory } from "react-router-dom";
+import { getUserById } from "../../services/users";
+import { ThemeProvider } from "styled-components";
+import { lightTheme, darkTheme, GlobalStyle } from "../../components/themes";
 import { LateralBar, InputBox, InputField, PostBoxStyle } from "./styled";
 import Switch from "react-switch";
-import {feedPost, getFeed} from "../../services/feed";
+import { feedPost, getFeed } from "../../services/feed";
 import InputFeed from "../../components/inputFeed";
 import logOut from "../../assets/log-out.svg";
 import home from "../../assets/home.svg";
@@ -21,11 +21,11 @@ interface IFeed {
 }
 
 const MainPage = () => {
-  const history = useHistory()
-  const id = localStorage.getItem('id')
+  const history = useHistory();
+  const id = localStorage.getItem("id");
 
-  const [name,setName] = useState<string>()
-  const [username,setUserName] = useState<string>()
+  const [name, setName] = useState<string>();
+  const [username, setUserName] = useState<string>();
   const [feed, setFeed] = useState<IFeed[]>([
     {
       text: "",
@@ -35,99 +35,110 @@ const MainPage = () => {
     },
   ]);
 
-  const changeName = async () =>{
-    const response = await getUserById(id as string)
-    let finalName = response?.data.name
-    const username = response?.data.username
-    finalName = finalName?.replaceAll(" ","\n");
+  const changeName = async () => {
+    const response = await getUserById(id as string);
+    let finalName = response?.data.name;
+    const username = response?.data.username;
+    finalName = finalName?.replaceAll(" ", "\n");
     setName(finalName);
-    setUserName(username)
-  }
+    setUserName(username);
+  };
 
   const getFeedData = async () => {
     const response = await getFeed();
     setFeed(response.feed);
   };
 
-
-  const handleLogin = async() => {
+  const handleLogin = async () => {
     localStorage.clear();
-    history.push('/login')
-  }
-
-  const goProfile = async() => {
-    history.push('/profile')
-  }
-
-  const [theme, setTheme] = useState(localStorage.getItem('theme'));
-  const [isDark, setIsDark] = useState(false);
-  localStorage.setItem('theme', theme!);
-
-  const changeTheme = () => {
-    
-    setIsDark(!isDark);
-    setTheme(isDark ? 'dark' : 'light');
-    
+    history.push("/login");
   };
 
+  const goProfile = async () => {
+    history.push("/profile");
+  };
 
-  useEffect (() => { 
+  const [theme, setTheme] = useState(localStorage.getItem("theme"));
+  const [isDark, setIsDark] = useState(false);
+  localStorage.setItem("theme", theme!);
+
+  const changeTheme = () => {
+    setIsDark(!isDark);
+    setTheme(isDark ? "dark" : "light");
+  };
+
+  useEffect(() => {
     getFeedData();
     changeName();
     try {
-      localStorage.getItem('theme')  
+      localStorage.getItem("theme");
     } catch (error) {
-      localStorage.setItem('theme', 'light')
+      localStorage.setItem("theme", "light");
     }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  },[]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <div className="container">
-      <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
+      <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
         <GlobalStyle />
-          <LateralBar >
-            <div className="infoBox">
-              <div className="nameBox">
-                {name}
-                <br/>
-                {username}
-              </div>
+        <LateralBar>
+          <div className="infoBox">
+            <div className="nameBox">
+              {name}
+              <br />
+              {username}
             </div>
-            <div className="optionsBox">
-              <Switch 
-                className="switch"
-                onChange={() => {changeTheme()}} 
-                checked={theme==='dark'} 
-                uncheckedIcon
-              />
-              <div className="svgBtn">
-                <img src={home} alt="home"/>
-                <p>Home</p>
-              </div>
-              <div className="svgBtn profile" onClick={() => {goProfile()}}>
-                <img src={userProfile} alt="home"/>
-                <p>Perfil</p>
-              </div>
-              <div className="svgBtn logout"  onClick={() => {handleLogin()}}>
-                <img src={logOut} alt="logout"/>
-                <p>Sair</p>
-              </div>
+          </div>
+          <div className="optionsBox">
+            <Switch
+              className="switch"
+              onChange={() => {
+                changeTheme();
+              }}
+              checked={theme === "dark"}
+              uncheckedIcon
+            />
+            <div className="svgBtn">
+              <img src={home} alt="home" />
+              <p>Home</p>
             </div>
-          </LateralBar>
+            <div
+              className="svgBtn profile"
+              onClick={() => {
+                goProfile();
+              }}
+            >
+              <img src={userProfile} alt="home" />
+              <p>Perfil</p>
+            </div>
+            <div
+              className="svgBtn logout"
+              onClick={() => {
+                handleLogin();
+              }}
+            >
+              <img src={logOut} alt="logout" />
+              <p>Sair</p>
+            </div>
+          </div>
+        </LateralBar>
 
-            <div className="postBox">
-              <InputFeed />
-              <div className="scrollBox">
-                {feed.length !== 0 &&
-                  feed.map((feedPost) => (
-                    <PostBox
-                      username={feedPost.userId.username}
-                      text={feedPost.text}
-                    />
-                  ))}
-              </div>
+        <div>
+          <div className="postBox">
+            <InputFeed />
+            <div className="scrollBox">
+              {feed.length !== 0 &&
+                feed.map((feedPost) => (
+                  <PostBox
+                    username={feedPost.userId.username}
+                    text={feedPost.text}
+                    postId={feedPost._id}
+                  />
+                ))}
             </div>
+          </div>
+        </div>
       </ThemeProvider>
     </div>
   );
--- app/src/services/like.ts
+++ app/src/services/like.ts
@@ -1,6 +1,8 @@
 import api from "./api";
+import axios from 'axios';
 
 const token = localStorage.getItem("token");
+const baseUrl = 'http://localhost:3001';
 
 const getLike = async () => {
   const response = await api.get("/like", {
@@ -9,4 +11,19 @@ const getLike = async () => {
   return response.data;
 };
 
-export default getLike;
+const postLike = async (values:any) => {
+  try {
+      const response = await axios.request({
+          url: `${baseUrl}/like`,
+          method: 'post',
+          headers: { Authorization: `Bearer ${token}` },
+          data: values
+      })
+      return response;
+  } catch (error) {
+      //console.error(`Falha na requisição: ${error}`);
+      throw new Error(error.response.data.message);
+  }
+}
+
+export {getLike, postLike};
